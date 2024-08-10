60c3472139c80e0ab4f5ad7ca5a97be81ee17fa6
Kaike da Silva Sathler <104788755+KaikeSathler@users.noreply.github.com>
2024-08-10 12:19:55 -0300

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
 import NotFound from '@/views/NotFoud.vue'
 import TopCenas from '@/views/TopCenas.vue'
 import Videos from '@/views/Videos.vue'
