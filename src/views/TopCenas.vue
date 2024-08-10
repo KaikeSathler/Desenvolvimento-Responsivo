8d7fb55d8e815c4d37cb32a8ea2d90c9d55cdd5a
Kaike da Silva Sathler <104788755+KaikeSathler@users.noreply.github.com>
2024-08-10 12:03:28 -0300

    Update TopCenas.vue

--- src/views/TopCenas.vue
+++ src/views/TopCenas.vue
@@ -1,20 +1,5 @@
-<script lang="ts">
-import '@/assets/style/StyleComponents.scss'
-export default {
-  name: 'TopCenas'
-}
+<script setup lang="ts">
+import TopCenasComponents from '@/components/TopCenas.vue'
 </script>
-<template>
-  <div class="title">
-    <h1>Top cenas breaking Bad</h1>
-    <h2>Essa são as Melhores cenas do seriado americano e latino Breaking Bad</h2>
-  </div>
-  <div class="image">
-    <img src="/image/Breaking-Bad-1.webp"/>
-    <img src="/image/1_gDOd2Hy.jpg"/>
-    <img src="/image/1516367128_442397_1516372173_album_normal.jpg"/>
-    <img src="/public/image/image3.jpg"/>
-    <img src="/public/image/image4.jpg"
-
-  </div>
+<TopCenasComponents />
 </template>
