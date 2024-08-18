f36d8dd34a53ccffe410e2c6272d3d4de1622038
Kaike da Silva Sathler <104788755+KaikeSathler@users.noreply.github.com>
2024-08-18 01:52:44 -0300

    Update 18/08 - Navbar Responsive

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,10 +1,23 @@
 <script setup lang="ts">
 import '@/assets/style/StyleComponents.scss'
 import { RouterLink } from 'vue-router'
+import { ref } from 'vue';
+
+const isMenuOpen = ref(false)
+
+function toggleMenu() {
+  isMenuOpen.value = !isMenuOpen.value
+}
 </script>
+
 <template>
-  <nav>
-    <ul>
+  <nav  id="nav">
+    <div class="nav-content">
+      <button class="navbar-button" @click="toggleMenu">
+        <i style="font-size: 1.5rem; color: white;" class="fa-solid fa-bars"></i>
+      </button>
+    </div>
+    <ul :class="{ 'nav-menu': true, 'open': isMenuOpen }">
       <router-link to="/">Páginas Inicial</router-link>
       <router-link to="topcenas">Top Cenas</router-link>
       <router-link to="videos">Videos</router-link>
